f9741a01e7fe00a9a38681655bdaf308846e5f1e
designdnt <thomas.doubek@gmail.com>
2019-04-15 15:57:18 +0200

    fix small bugfixes

--- modules/video_list/tpl.php
+++ modules/video_list/tpl.php
@@ -4,6 +4,27 @@
 	   <div class="wrap">
 		  <div class="grids">
 			 
+			 
+			 <div class="grid-12">
+                        <!-- CLANKY  -->
+                        <h2><?php echo $data['article']['name']?></h2>
+                        <ul class="meta">
+                           <li class="usericon">
+                              <a href="<?php echo WWW_PATH."a/14862"?>">developer</a>
+                           </li>
+                           <li class="categoryicon">
+							  <a href="<?php echo WWW_PATH."a/15011"?>">odhad ceny</a>
+                           </li>
+                           <li class="tagsicon"> 
+                              <a href="<?php echo WWW_PATH."".$data['webhook'][1]; ?>" rel="tag">prejsť na zoznam</a> 
+                           </li>
+                        </ul>
+						<div class="col-xs-12">
+							<?php echo $data['article']['perex']?>
+						</div>
+
+                     </div>
+					 
 			 <div class="grid-12">
 			 <?php 
                   $img = new Image();
